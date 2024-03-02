03e239a766a4dd816a8b8a7baf0a0250a0f04420
Karlos348 <karoldobkowski3@gmail.com>
2024-03-02 15:25:03 +0100

    add method with input

--- programs/dblik/src/lib.rs
+++ programs/dblik/src/lib.rs
@@ -9,7 +9,25 @@ pub mod dblik {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
+
+    pub fn set_data(ctx: Context<SetData>, data: u64) -> Result<()> {
+        ctx.accounts.my_account.data = data;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account]
+#[derive(Default)]
+pub struct MyAccount {
+    data: u64
+}
+
+#[derive(Accounts)]
+pub struct SetData<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>
+}
+
