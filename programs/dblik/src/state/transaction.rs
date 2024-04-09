e9101d765d803c8051a114ed580bebddaf5e302d
Karlos348 <karoldobkowski3@gmail.com>
2024-04-09 22:28:44 +0200

    reorganize tests

--- programs/dblik/src/state/transaction.rs
+++ programs/dblik/src/state/transaction.rs
@@ -85,3 +85,24 @@ pub enum TransactionErrors {
 //         //
 //     }
 // }
+
+#[cfg(test)]
+pub mod tests {
+    use anchor_lang::{solana_program::pubkey::Pubkey, AnchorSerialize, Discriminator};
+    use crate::{Transaction, TransactionAccount};
+
+    #[test]
+    fn it_works() {
+        let customer_bytes: [u8; 32] = [1; 32];
+        let customer_pubkey : Pubkey = Pubkey::new_from_array(customer_bytes);
+        let discriminator = Transaction::discriminator(); 
+
+        let serialized_transaction = 
+            <anchor_lang::prelude::Account<'_, Transaction> as TransactionAccount>
+            ::new_serialized_transaction(customer_pubkey);
+
+        assert_eq!([11 ,24 ,174 ,129 ,203 ,117 ,242 ,23], discriminator);
+        // assert_eq!(serialized_transaction[0..8], discriminator);
+        // assert_eq!(serialized_transaction[9..9+32], customer_bytes);
+    }
+}
