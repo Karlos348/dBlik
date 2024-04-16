3a2d96fc6fb2433618d8ac47b7806bdfd1cdd457
Karlos348 <karoldobkowski3@gmail.com>
2024-04-16 22:10:56 +0200

    add a refundable charge for hooking into transaction to prevent auto requests -  part 1

--- programs/dblik/src/ctx/request_payment.rs
+++ programs/dblik/src/ctx/request_payment.rs
@@ -1,5 +1,5 @@
 use crate::*;
-use self::consts::{BASIC_TRANSACTION_SIZE, FREE_TRANSACTION_MESSAGE_SIZE};
+use self::consts::{BASIC_TRANSACTION_SIZE, FREE_TRANSACTION_MESSAGE_SIZE, RETURNABLE_STORE_FEE};
 
 #[derive(Accounts)]
 #[instruction(amount: u64, message: String)]
@@ -24,6 +24,12 @@ impl<'info> RequestPayment<'info> {
     pub fn process(&mut self, amount: u64, message: String) -> Result<()> {  
         let store = self.signer.signer_key();
         require!(store.is_some(), RequestPaymentErrors::NoStoreKey);
+
+        let transaction_account_info = self.transaction.to_account_info();
+        let store_account_info = self.signer.to_account_info();
+
+        **store_account_info.try_borrow_mut_lamports()? -= RETURNABLE_STORE_FEE;
+        **transaction_account_info.try_borrow_mut_lamports()? += RETURNABLE_STORE_FEE;
 
         self.transaction.assign_store(TimeProvider, *store.unwrap(), amount, message)
     }
--- programs/dblik/src/utils/consts.rs
+++ programs/dblik/src/utils/consts.rs
@@ -1,6 +1,7 @@
-use anchor_lang::solana_program::pubkey::Pubkey;
+use anchor_lang::solana_program::{native_token::LAMPORTS_PER_SOL, pubkey::Pubkey};
 
 pub const FREE_TRANSACTION_MESSAGE_SIZE: usize = 10;
 pub const BASIC_TRANSACTION_SIZE: usize = 93 + FREE_TRANSACTION_MESSAGE_SIZE;
 pub const TRANSACTION_EXPIRATION_TIME_IN_SECONDS: i64 = 120;
-pub const DEFAULT_PUBKEY : Pubkey = Pubkey::new_from_array([0; 32]);
+pub const DEFAULT_PUBKEY: Pubkey = Pubkey::new_from_array([0; 32]);
+pub const RETURNABLE_STORE_FEE: u64 = LAMPORTS_PER_SOL/200;
