5f0db17d6fac2c116cd807de5c4665085bb39ec0
Karlos348 <karoldobkowski3@gmail.com>
2024-04-24 23:55:17 +0200

    add overlap zone definition

--- app/dblik/utils/transaction.ts
+++ app/dblik/utils/transaction.ts
@@ -1,14 +1,35 @@
 import { generateCode } from "./code";
 
 const TRANSACTION_WINDOW_IN_MINUTES: number = 10;
+const TRANSACTION_EXPIRATION_TIME_IN_SECONDS: number = 120;
+const MINUTES_TO_SECONDS_MULTIPLIER: number = 60;
 
 export function generateSeedForCustomer() {
-    return generateCode().toString() + getRoundedUtcDate();
+    return generateCode().toString() + getRoundedUtcDate(new Date());
 }
 
-function getRoundedUtcDate() {
+export function generateSeedsForStore(code: number)
+{
     const now = new Date();
-    const minutes = now.getUTCMinutes();
-    now.setMinutes(minutes + TRANSACTION_WINDOW_IN_MINUTES - (minutes % TRANSACTION_WINDOW_IN_MINUTES));
-    return now.toISOString().replace(/\D/g, '').slice(0, 12);
+    if(isInOverlapZone(now))
+    {
+        // todo: check also previous rounded date
+        return getRoundedUtcDate(now);
+    }
+
+    return getRoundedUtcDate(now);
+}
+
+function getRoundedUtcDate(date: Date) {
+    const minutes = date.getUTCMinutes();
+    date.setMinutes(minutes + TRANSACTION_WINDOW_IN_MINUTES - (minutes % TRANSACTION_WINDOW_IN_MINUTES));
+    // todo: add minutes instead of set (?)
+    
+    return date.toISOString().replace(/\D/g, '').slice(0, 12);
+}
+
+function isInOverlapZone(date: Date) {
+    const minutes = date.getUTCMinutes();
+    const seconds = date.getUTCSeconds();
+    return ((minutes % TRANSACTION_WINDOW_IN_MINUTES) * MINUTES_TO_SECONDS_MULTIPLIER) + seconds < TRANSACTION_EXPIRATION_TIME_IN_SECONDS;
 }
