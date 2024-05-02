05fd637c6cb202cae4cb69057be0c0aa5b8f332a
Karlos348 <karoldobkowski3@gmail.com>
2024-05-02 23:19:11 +0200

    add store page

---
+++ app/dblik/app/store/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Home() {
+
+  return (
+<main className="flex min-h-screen flex-col items-center justify-between p-64">
+  STORE<br/>
+  tbd
+</main>
+  )
+}
