9bd64df5bddb8cc4bab4dc03e3f65ede6fb8663d
Karlos348 <karoldobkowski3@gmail.com>
2024-05-13 23:04:39 +0200

    fix globals.css in store

--- app/store/app/layout.tsx
+++ app/store/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AppWrapper } from ".";
-//import "../globals.css";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
