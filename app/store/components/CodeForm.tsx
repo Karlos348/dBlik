875f23d72424fc9977aaafe4b8d328eaba29d13b
Karlos348 <karoldobkowski3@gmail.com>
2024-06-03 18:45:51 +0200

    modify pay button

--- app/store/components/CodeForm.tsx
+++ app/store/components/CodeForm.tsx
@@ -68,6 +68,10 @@ export function CodeForm() {
           width: fit-content;
         }
 
+        .button-container:hover {
+          box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+        }
+
         .logo {
           background-color: transparent;
           padding: 0px;
