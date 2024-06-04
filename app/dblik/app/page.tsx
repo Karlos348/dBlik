dcf1df73cc57d652819e83c81fb67dd829dcd833
Karlos348 <karoldobkowski3@gmail.com>
2024-06-04 23:53:40 +0200

    add information about success of the transaction

--- app/dblik/app/page.tsx
+++ app/dblik/app/page.tsx
@@ -4,6 +4,7 @@ import { GenerateCodeButton } from "@/components/GenerateCodeButton";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { IncomingTransactionBox } from "@/components/IncomingTransactionBox";
 import Timer from "@/components/Timer";
+import { TransactionState } from "@/models/transaction";
 
 export default function Home() {
   const transaction = useTransaction();
@@ -12,8 +13,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
       {connected ? <>
-        {transaction.code !== undefined 
-          ? <div className="flex items-center justify-center w-96 h-16 text-4xl px-4 py-2 border border-gray-400 text-gray-700 rounded-md hover:shadow-md focus:outline-none focus:ring focus:ring-gray-200 mt-12 gray-border">
+        {transaction.code !== undefined && transaction.transaction?.state !== TransactionState.Succeed
+          ? <div className="flex items-center justify-center w-96 h-16 text-4xl px-4 py-2 border border-gray-400 text-gray-700 rounded-md focus:outline-none focus:ring focus:ring-gray-200 mt-12 gray-border">
               <span className='uppercase tracking-widest'>{transaction.code}</span>
             </div> 
           : <></>}
